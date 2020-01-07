5ed78f0ff35bc383bc55fa95cda3c95b41d557ad
Mubin <mbnmughal30@gmail.com>
2020-01-07 08:09:36 +0500

    Updates

---
+++ precache-manifest.b7ed5bfb583cebb39be5e0402c5178fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a3ef2f6836fdd70260437e582a88e6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4abb7e1d4a9c3b49220d",
+    "url": "/monsters-rolodex/static/css/main.8bcfdf83.chunk.css"
+  },
+  {
+    "revision": "55ee059b2988e93ca287",
+    "url": "/monsters-rolodex/static/js/2.279358c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.279358c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4abb7e1d4a9c3b49220d",
+    "url": "/monsters-rolodex/static/js/main.2e6550a9.chunk.js"
+  },
+  {
+    "revision": "d90ac9b9402da2dda087",
+    "url": "/monsters-rolodex/static/js/runtime-main.f8e4036a.js"
+  }
+]);
